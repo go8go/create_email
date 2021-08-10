a264191ef2b2acc2a064057f534dc7c9f6cb6c29
Touhidul Shawan <touhidulshawan@gmail.com>
2021-08-10 16:27:03 +0600

    automate generate usernames

--- random_username_gen.py
+++ random_username_gen.py
@@ -1,50 +1,56 @@
-# pandas pip install pandas
-# numpy pip install numpy
-# xlrd pip install xlrd
-# openpyxl pip install openpyxl
+# TODO: FIRST INSTALL THIS PACKAGES BEFORE RUN THIS FILE
+
+# pandas -> pip install pandas
+# numpy -> pip install numpy
+# xlrd -> pip install xlrd
+# openpyxl -> pip install openpyxl
+# faker -> pip install Faker
 
 import numpy as np
 import pandas as pd
+from faker import Faker
 
-# read data from file
-# if want to read excel file then change it to pd.read_excel()
-all_data = pd.read_csv("test_data.csv")  # read only csv file
+fake = Faker()
 
-# make dataframe
-all_data_df = pd.DataFrame(all_data)
+# how many user name you want to generate
+ammount_of_names = int(input("How many names you want to generate: "))
 
-# get first_name column data
+step = 1
+first_name = []
+last_name = []
 
-first_name = all_data_df["first_name"]
-first_name = first_name.str.lower()  # converts all data to lowercase
-# get last_name column data
+try:
+    # generate and first name modify and store in the lists
+    while step <= ammount_of_names:
+        name = [str(name) for name in fake.name().split()]
+        first_name.append(name[0].lower())
+        last_name.append(name[1].lower())
+        step = step + 1
 
-last_name = all_data_df["last_name"]
-last_name = last_name.str.lower()  # converts all data to lowercase
+    # change first_name and last_name to panda series
+    first_name = pd.Series(first_name, name="first_name")
+    last_name = pd.Series(last_name, name="last_name")
 
-# Generate random number
+    # Generate random number
+    random_number = np.random.randint(2999, 9999, size=ammount_of_names)
 
-random_number = np.random.randint(2999, 9999, size=first_name.size)
-random_number_df = pd.DataFrame(random_number, columns=["random_numbers"])
+    # Convert random numbers to pandas series
+    numbers = pd.Series(random_number, name="random_numbers")
 
-numbers = random_number_df["random_numbers"]
+    # combine first_name and last_name and numbers make username
+    combined_value = pd.concat([first_name, last_name], axis=1)
 
-# combine first_name and last_name
+    usernames = combined_value.assign(username=combined_value.first_name.astype(
+        str) + "." + combined_value.last_name.astype(str) + numbers.astype(str)
 
-# combined_name = first_name + "." + last_name + numbers
-# filtered_data_frame = pd.DataFrame(combined_name, columns=["Name"])
-filtered_data_frame = pd.concat([first_name, last_name, numbers], axis=1)
+    )
 
-filtered_data_frame = filtered_data_frame.assign(combined_value=filtered_data_frame.first_name.astype(
-    str) + "." + filtered_data_frame.last_name.astype(str) + filtered_data_frame.random_numbers.astype(str)
+    try:
+        usernames.to_excel("output_names.xlsx",
+                           sheet_name='Person`s Name', index=False)
 
-                                                 )
-
-final_data = filtered_data_frame["combined_value"]
-
-final_data_frame = pd.DataFrame(final_data)
-
-final_data = pd.concat([final_data_frame, all_data_df], axis=1, ignore_index=True)
-
-final_data.to_excel("output_names.xlsx",
-                    sheet_name='Person`s Name', index=False)
+        print("Data saved to [output_names.xlsx] file successfully")
+    except:
+        print("Data does not save into file")
+except:
+    print("Something wrong!! Please try again.")
